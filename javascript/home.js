efa8bbd5095976a2f5713e46e49ef53f519c4b26
Peonie Lin <98808956+peonielin@users.noreply.github.com>
2026-03-04 14:31:08 +1100

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,30 @@
 function changeToCross(x) {
   x.classList.toggle("change");
 }
+
+/* const importArray = [
+    { newGoalName: "bike" ,
+     newTargetNum: 1200 ,
+    newStartingNum: 1000 ,
+    newGoalUnits: "km" ,
+    newGoalIncrement: 1 },
+        { newGoalName: "bike" ,
+     newTargetNum: "1200" ,
+    newStartingNum: 160 ,
+    newGoalUnits: "km" ,
+    newGoalIncrement: "1" },
+        { newGoalName: "bike" ,
+     newTargetNum: "1200" ,
+    newStartingNum: 400 ,
+    newGoalUnits: "km" ,
+    newGoalIncrement: "1" }] */
+    
+const goalListContainer = document.querySelector(".goals-container");
+
+for(let i =0; i<importArray.length;i++){
+  const item = importArray[i];
+  const newDiv = document.createElement('div');
+  newDiv.className = "singular-goal";
+  newDiv.innerHTML = item.newGoalName +" "+ item.newStartingNum +"/" +item.newTargetNum+ item.newGoalUnits + " "+ ((item.newStartingNum/item.newTargetNum)*100)+ "%" 
+  goalListContainer.appendChild(newDiv);
+}
